fb9d552306cc35fd98327c515f69d9eb91b1c11e
Amr M. Kayid <amrkayid2027@gmail.com>
2018-09-06 00:02:56 +0200

    Tags

--- Gemfile
+++ Gemfile
@@ -5,8 +5,8 @@ ruby '2.5.1'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.0'
-# Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+# Use postgresql as the database for Active Record
+gem 'pg'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
@@ -33,12 +33,13 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
-gem 'annotate'
 gem 'devise'
+gem 'annotate'
 gem 'jquery-rails'
-gem 'will_paginate', '~> 3.1.0'
 gem 'bootstrap', '~> 4.1.3'
 gem 'friendly_id', '~> 5.2.0' # Note: You MUST use 5.0.0 or greater for Rails 4.0+
+gem 'will_paginate', '~> 3.1.0'
+gem 'acts-as-taggable-on', '~> 6.0'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
--- app/models/post.rb
+++ app/models/post.rb
@@ -16,33 +16,42 @@
 #
 
 class Post < ApplicationRecord
+  acts_as_taggable # Alias for acts_as_taggable_on :tags
 
-	extend FriendlyId
-  	friendly_id :title, use: :slugged
+  extend FriendlyId
+  friendly_id :title, use: :slugged
 
-  	belongs_to :author
-  	
-    scope :most_recent, -> { order(published_at: :desc) }
-    scope :published, -> { where(published: true) }
+  belongs_to :author
 
-  	def should_generate_new_friendly_id?
-  		title_changed?
-  	end
+  PER_PAGE = 3
 
-  	def display_day_published
+  scope :most_recent, -> { order(published_at: :desc) }
+  scope :published, -> { where(published: true) }
+  scope :recent_paginated, -> (page) { most_recent.paginate(page: page, per_page: PER_PAGE) }
+  scope :with_tag, -> (tag) { tagged_with(tag) if tag.present? }
+
+  scope :list_for, -> (page, tag) do
+    recent_paginated(page).with_tag(tag)
+  end
+
+  def should_generate_new_friendly_id?
+    title_changed?
+  end
+
+  def display_day_published
     if published_at.present?
       "Published #{published_at.strftime('%-b %-d, %Y')}"
     else
       "Not published yet."
     end
-    end
+  end
 
-    def publish
-      update(published: true, published_at: Time.now)
-    end
+  def publish
+    update(published: true, published_at: Time.now)
+  end
 
-    def unpublish
-      update(published: false, published_at: nil)
-    end
+  def unpublish
+    update(published: false, published_at: nil)
+  end
 
 end
